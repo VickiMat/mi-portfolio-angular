cc22128fb894ee8a4c3b125c630c38aeaae06d9f
VickiMat <94490367+VickiMat@users.noreply.github.com>
2022-04-15 01:31:10 -0300

    botones experiencia laboral

--- portfolio-angular/src/app/components/experience/experience.component.ts
+++ portfolio-angular/src/app/components/experience/experience.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from 'src/app/services/portfolio.service';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.css']
 })
+
 export class ExperienceComponent implements OnInit {
   experiencia: any;
+  faEdit = faEdit;
+  faAdd = faPlus;
+  faTrash = faTrash;
+
   constructor(private miServicio: PortfolioService) { }
 
   ngOnInit(): void {this.miServicio.obtenerDatosExperiencia().subscribe(data => {
